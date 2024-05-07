24056222de616e2b8a98b1666f1f9f53bda59f71
Phillip-Rek <philliprekhotho@gmail.com>
2024-05-07 21:10:27 +0200

    New File: component to indicate when a video/song is being loaded

---
+++ src/loadingIndicator/loadingIndicator.ts
@@ -0,0 +1,22 @@
+import { createHTMLElement } from "../utils/utils.js";
+
+class LoadingIndicator {
+    private container = createHTMLElement("div", { id: "loading-indicator" });
+    private loadingIndicatorElement = createHTMLElement("div");
+
+    constructor() {
+        this.loadingIndicatorElement.textContent = "Loading!"
+        this.container.appendChild(this.loadingIndicatorElement);
+
+        this.loadEnd();
+    }
+
+    getContainer() { return this.container }
+
+
+    loadStart() { this.container.style.display = "flex" }
+
+    loadEnd() { this.container.style.display = "none" }
+}
+
+export const loadingIndicator = new LoadingIndicator();
