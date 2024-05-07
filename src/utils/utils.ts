dd6bfd70383546736d08a8226e9161938c965e68
Phillip-Rek <philliprekhotho@gmail.com>
2024-05-07 19:35:08 +0200

    Feat: new utility function to convert time form seconds to 24 Hour Format

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -25,3 +25,40 @@ export function createHTMLElement(name: string, props: {
 
     return element;
 }
+
+
+export function timeFromSecondsToMinutesSeconds(time: number): string {
+    if (Number.isNaN(time)) return "";
+
+    let sec = "";
+    let mins = "";
+    let hrs = "";
+
+    if (time <= 60) {
+        mins = `00`;
+        if (time < 10)
+            sec = `0${Math.floor(time)}`;
+        else
+            sec = `${Math.floor(time)}`;
+
+        return mins + ":" + sec;
+    }
+    else if (time <= 60 * 60) {
+        time /= 60;
+        if (Math.floor(time) < 10)
+            mins = `0${Math.floor(time)}`;
+        else
+            mins = `${Math.floor(time)}`;
+
+        if ((time - Math.floor(time)) * 60 < 10)
+            sec = `0${Math.floor((time - Math.floor(time)) * 60)}`;
+        else
+            sec = `${Math.floor((time - Math.floor(time)) * 60)}`;
+
+        return mins + ":" + sec;
+    }
+    else {
+        hrs = `${Math.floor(time / (60 * 60))}`;
+        return hrs + ":" + timeFromSecondsToMinutesSeconds((time / (60 * 60) - Math.floor(time / (60 * 60))) * 60 * 60);
+    }
+}
