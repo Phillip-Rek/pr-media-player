b08ce38806d2cf00b6327e5b95268ae719afce8e
Phillip-Rek <philliprekhotho@gmail.com>
2024-05-08 13:18:42 +0200

    feat: Play method for VideoScreen component

--- src/videoScreen/videoScreen.ts
+++ src/videoScreen/videoScreen.ts
@@ -1,3 +1,5 @@
+import { bottomTitleSection } from "../bottomTitleSection/bottomTitleSection.js";
+import { Track } from "../playlist/playlist.js";
 import { createHTMLElement } from "../utils/utils.js";
 
 class VideoScreen {
@@ -5,6 +7,15 @@ class VideoScreen {
     private videoContainer = createHTMLElement("div", { id: "video-container" });
 
     constructor() {
+    }
+
+    getVideoContainer() { return this.videoContainer }
+    getVideoElement() { return this.videoElement; }
+
+
+    play(song: Track) {
+        this.videoElement.src = song.url;
+        bottomTitleSection.updateTitle(song.title);
     }
 }
 
