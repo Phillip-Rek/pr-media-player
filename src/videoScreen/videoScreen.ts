5b28aac982093d3a352e4a4721a8090cf4c76ee2
Phillip-Rek <philliprekhotho@gmail.com>
2024-05-08 15:09:51 +0200

    feat: added method to toggle pause on the video

--- src/videoScreen/videoScreen.ts
+++ src/videoScreen/videoScreen.ts
@@ -1,6 +1,7 @@
 import { bottomTitleSection } from "../bottomTitleSection/bottomTitleSection.js";
-import { Track } from "../playlist/playlist.js";
+import { playList, Track } from "../playlist/playlist.js";
 import { createHTMLElement } from "../utils/utils.js";
+import { videoNavigation } from "../videoNavigation/videoNavigation.js";
 
 class VideoScreen {
     private videoElement = <HTMLVideoElement>createHTMLElement("video", { id: "video", attributes: { autoplay: "true" } });
@@ -12,10 +13,28 @@ class VideoScreen {
     getVideoContainer() { return this.videoContainer }
     getVideoElement() { return this.videoElement; }
 
-
     play(song: Track) {
         this.videoElement.src = song.url;
         bottomTitleSection.updateTitle(song.title);
+    }
+
+    togglePause(playButton: HTMLElement) {
+        bottomTitleSection.updateTitle(playList.currentPlayingSong?.title);
+        if (this.videoElement.paused) {
+            this.videoElement.play()
+                .then(res => {
+                    playButton.className = "fa fa-pause";
+                })
+                .catch(err => {
+                    playButton.className = "fa fa-play";
+                })
+        }
+        else {
+            videoScreen.videoElement.pause();
+            playButton.className = "fa fa-play";
+        }
+
+        videoNavigation.setDuration(this.videoElement.duration);
     }
 }
 
